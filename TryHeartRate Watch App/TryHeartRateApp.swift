06a8e7e7641206f34cf509ec505b53f19994fdec
CallistaEve <70805652+CallistaEve@users.noreply.github.com>
2024-10-22 09:16:19 +0700

    Initial Commit

---
+++ TryHeartRate Watch App/TryHeartRateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryHeartRateApp.swift
+//  TryHeartRate Watch App
+//
+//  Created by Evelyn Callista Yaurentius on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TryHeartRate_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TryHeartRate/TryHeartRateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryHeartRateApp.swift
+//  TryHeartRate
+//
+//  Created by Evelyn Callista Yaurentius on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TryHeartRateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
